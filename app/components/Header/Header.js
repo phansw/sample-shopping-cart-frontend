ee65773581aecb089b235c3cdd38c1a92895ea1f
phansw <applesolame@hotmail.com>
2019-03-14 23:17:17 +0800

    Add basic header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,23 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Banner from './images/banner.jpg';
+import React, { Component } from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import './style.scss';
 
-class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+class Header extends Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div className="header">
-        <a href="https://twitter.com/flexdinesh">
-          <img src={Banner} alt="react-redux-boilerplate - Logo" />
-        </a>
-        <div className="nav-bar">
-          <Link className="router-link" to="/">
-            Home
-          </Link>
-          <Link className="router-link" to="/features">
-            Features
-          </Link>
-        </div>
+      <div className="root">
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" color="inherit" className="grow">
+              The Corner Bookstore
+            </Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
