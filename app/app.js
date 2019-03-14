9d2f459d980ffa4f8d9460ad820ea6f8ddb290d4
phansw <applesolame@hotmail.com>
2019-03-14 20:57:28 +0800

    Change global font to roboto

--- app/app.js
+++ app/app.js
@@ -30,12 +30,12 @@ import 'styles/theme.scss';
 
 import configureStore from './configureStore';
 
-// Observe loading of Open Sans (to remove open sans, remove the <link> tag in
+// Observe loading of Roboto (to remove roboto, remove the <link> tag in
 // the index.html file and this observer)
-const openSansObserver = new FontFaceObserver('Open Sans', {});
+const robotoObserver = new FontFaceObserver('Roboto', {});
 
-// When Open Sans is loaded, add a font-family using Open Sans to the body
-openSansObserver.load().then(() => {
+// When Roboto is loaded, add a font-family using Roboto to the body
+robotoObserver.load().then(() => {
   document.body.classList.add('fontLoaded');
 }, () => {
   document.body.classList.remove('fontLoaded');
