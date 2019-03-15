419e7b3ccff45ca615fbf51bd128bfae4fed5df4
phansw <applesolame@hotmail.com>
2019-03-16 02:47:30 +0800

    Add logout function in header

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -6,21 +7,49 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import './style.scss';
 
-class Header extends Component { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <div className="root">
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" color="inherit" className="grow">
-              The Corner Bookstore
-            </Typography>
-            <Button color="inherit" component={Link} to="/login">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
+const Header = ({ logout, userToken, username }) => {
+  let ActionButton = null;
+  if (userToken && userToken.length > 0) {
+    ActionButton = (
+      <Button
+        color="inherit"
+        onClick={() => logout()}
+      >
+        Logout ({username.split('@')[0]})
+      </Button>
+    );
+  } else {
+    ActionButton = (
+      <Button color="inherit" component={Link} to="/login">
+        Login
+      </Button>
     );
   }
-}
+
+
+  return (
+    <div className="root">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className="grow">
+            The Corner Bookstore
+          </Typography>
+          {ActionButton}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
+
+Header.propTypes = {
+  logout: PropTypes.func.isRequired,
+  userToken: PropTypes.string,
+  username: PropTypes.string,
+};
+
+Header.defaultProps = {
+  userToken: null,
+  username: '',
+};
 
 export default Header;
--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,1 +1,16 @@
-export { default } from './Header';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { makeSelectUserToken, makeSelectUsername } from 'containers/App/selectors';
+import { loginRestart } from 'containers/App/actions';
+import Header from './Header';
+
+const mapDispatchToProps = (dispatch) => ({
+  logout: () => dispatch(loginRestart()),
+});
+
+const mapStateToProps = createStructuredSelector({
+  userToken: makeSelectUserToken(),
+  username: makeSelectUsername(),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
